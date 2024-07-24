1d54a43a9a59c5111cc0bd795c033844df44122e
WltrH <WltrHrng@gmail.com>
2024-07-24 12:38:43 +0700

    Debugging 1

--- pyanlalysis.py
+++ pyanlalysis.py
@@ -18,24 +18,34 @@ df = pd.read_json('/path/to/AccidentUS.json')
 # Titre de l'application
 st.title("Analyse scientifique avec Streamlit")
 
-# Ajoutez votre code d'analyse scientifique ici
+####################Setting######################
+page_title = "Accident USA Data Analysis"
+page_icon = ":bar_chart:"
+layout = "centered"
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
+
+# Container 1
+with st.container():
+    st.write("This is a container")
+    st.write("You can use it to organize your content")
+
+    m = fo.Map(location=[48.8566, 2.3522], zoom_start=12)
+    st.map(m)
+
+    # Display the data frame
+    st.write(df)
+
+# Container 2
+
+with st.container():
+
+    st.write("Ceci est un container")
+    st.write("Vous pouvez l'utiliser pour organiser votre contenu")
 
 
+    m = fo.Map(location=[48.8566, 2.3522], zoom_start=12)
+    st.map(m)
 
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
-
-plt.plot(x, y)
-st.pyplot(plt)
-
-# Exemple : Afficher une table de données
-
-data = {
-    'Nom': ['Alice', 'Bob', 'Charlie'],
-    'Âge': [25, 30, 35],
-    'Ville': ['Paris', 'Londres', 'New York']
-}
-
-df = pd.DataFrame(data)
-
-st.write(df)
+    # Afficher le data frame
+    st.write(df)
